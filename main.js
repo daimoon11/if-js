6924ba825de2547662fd265b6c43285f4c3a1d8d
Brovchyk Dmitriy <dimon1112995@gmail.com>
2023-01-15 19:51:06 +0300

    do task 8

---
+++ main.js
@@ -0,0 +1,13 @@
+// const user = student;
+// console.log(user);
+
+// const student = 'Dmitriy Brovchyck';
+// console.log(student);
+
+const arr = [2, 3, 5, 8];
+let resalt = 1;
+
+for (let i = 0; i < 4; i++) {
+  resalt = arr[i] * resalt;
+}
+console.log(resalt);
