9b7506cec01e82c4e384449b93fde0a67a693ab4
Brovchyk Dmitriy <dimon1112995@gmail.com>
2023-01-16 18:07:48 +0300

    created new ruls and test it

--- main.js
+++ main.js
@@ -4,12 +4,15 @@
 // const student = 'Dmitriy Brovchyck';
 // console.log(student);
 
-const arr = [2, 5, 8, 15, 0, 6, 20, 3];
-let resalt = 1;
+// const arr = [2, 5, 8, 15, 0, 6, 20, 3];
+// let resalt = 1;
 
-for (let i = 0; i < 4; i++) {
-  resalt = arr[i];
-  //   console.log(resalt);
-  // if (resalt > 5) if (resalt < 10)
-  console.log(resalt);
-}
+// for (let i = 0; i < 4; i++) {
+//   resalt = arr[i];
+//   //   console.log(resalt);
+//   // if (resalt > 5) if (resalt < 10)
+//   console.log(resalt);
+// }
+
+let user = 1;
+console.log(user);
