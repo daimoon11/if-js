56c367654f6621d417f81feb461d850216e91923
Brovchyk Dmitriy <dimon1112995@gmail.com>
2023-01-23 17:34:03 +0300

    created new branch

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-// const user = student;
-// console.log(user);
-
-// const student = 'Dmitriy Brovchyck';
-// console.log(student);
-
-// const arr = [2, 5, 8, 15, 0, 6, 20, 3];
-// let resalt = 1;
-
-// for (let i = 0; i < 4; i++) {
-//   resalt = arr[i];
-//   //   console.log(resalt);
-//   // if (resalt > 5) if (resalt < 10)
-//   console.log(resalt);
-// }
-
-const user = 1;
-console.log(user);
+let x = 1;
+const arr = [3, 5, 7, 8, 9];
+for (let i = 0; i < arr.length; ++i) {
+  x = x * arr[i];
+}
+console.log(x);
